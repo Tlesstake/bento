328ea73c7f8f5520727ce118cc3feeabd3983114
rrp5 <bobby_poulin@sfu.ca>
2020-09-18 03:00:46 -0700

    Fixed spacing on greetings

--- js/greeting.js
+++ js/greeting.js
@@ -3,10 +3,10 @@ var Hr = today.getHours();
 
 // Here you can change the greetings and your name
 var name = 'roland';
-var lateTxt = 'stay up for a little while longer';
-var morningTxt = 'did you stay up all night?';
-var afterTxt = 'eat something for lunch this time';
-var evenTxt = 'it\'s still pretty early';
+var lateTxt = ', stay up for a little while longer';
+var morningTxt = ', did you stay up all night?';
+var afterTxt = ', eat something for lunch this time';
+var evenTxt = ', it\'s still pretty early';
 
 if (Hr >= 0 && Hr < 6) {
     document.getElementById('greetings').innerText = lateTxt + name;
